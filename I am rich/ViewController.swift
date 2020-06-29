18ec805782b920c3768711c5fd6490ed4d74bf1a
kowalkowska-just <kowalkowska.just@gmail.com>
2020-06-29 17:40:22 +0200

    The complete app with all files

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by user on 24/03/2020.
+//  Copyright © 2020 Justyna Kowalkowska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.white
+        
+    }
+
+
+}
+
